42c9475c2212c2383fed28f3ca981ac0a6267b46
Samuel Ayodeji <balosamm2@gmail.com>
2022-05-07 00:36:00 +0100

    first commit

---
+++ tennis.js
@@ -0,0 +1,55 @@
+
+const p1 ={
+    score:0,
+    button:document.querySelector('#p1Button'),
+    display:document.querySelector('#first')
+}
+
+const p2 ={
+    score:0,
+    button:document.querySelector('#p2Button'),
+    display:document.querySelector('#second')
+}
+
+
+const resetGame=document.querySelector('#reset')
+const gameRange=document.querySelector('#range')
+let winningScore=3;
+let isGameOver=false;
+
+function scoreUpdate(player,opponet) {
+    if (!isGameOver) {
+        player.score +=1;
+        if (player.score===winningScore) {
+            isGameOver=true;
+            player.display.classList.add('has-text-success')
+            opponet.display.classList.add('has-text-danger')
+            player.button.disabled=true;
+            opponet.button.disabled=true;
+        }
+        player.display.textContent=player.score;
+    }
+}
+
+p1.button.addEventListener('click', function(){
+    scoreUpdate(p1,p2);
+})
+p2.button.addEventListener('click', function(){
+    scoreUpdate(p2,p1);
+})
+
+gameRange.addEventListener('change',function(){
+    winningScore=parseInt(this.value);
+reset()
+})
+
+resetGame.addEventListener('click',reset)
+function reset(){
+    isGameOver=false;
+    for (let p of [p1, p2]) {
+        p.score = 0
+        p.display.textContent = 0
+        p.display.classList.remove('has-text-success', 'has-text-danger')
+        p.button.disabled = false;
+    }
+}
